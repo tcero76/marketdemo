2ab2d6472a66c6299e1d12150681ca8fa187ec22
Leonardo Lastra Abufarne <leonardo.lastra@gmail.com>
2026-06-17 14:31:34 -0400

    secrets, dns

--- infra/docker/config/hydra/entrypoint.sh
+++ infra/docker/config/hydra/entrypoint.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
 set -eu  # ‘-e’ para salir en error, ‘-u’ para variables no definidas
 
-load_secret() {
-  local var_name="$1"
-  local secret_file="${var_name}_FILE"
-
-  if [ -f "$secret_file" ]; then
-    export "$var_name"="$(cat "$secret_file" | tr -d '\r\n')"
-    echo "Loaded secret for $var_name"
-  else
-    echo "Warning: secret file $secret_file not found. Skipping."
-  fi
-}
-
-load_secret POSTGRES_PASSWORD
+export POSTGRES_PASSWORD=$(cat "$POSTGRES_PASSWORD_FILE")
+export SECRETS_SYSTEM=$(cat "$SECRETS_SYSTEM_FILE")
 export DNS="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=disable&options=-c%20search_path%3Dmarketplace"
 
-exec "$@"
+exec hydra serve all --dev
