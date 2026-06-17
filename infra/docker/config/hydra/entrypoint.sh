eb0cf97af3881e1afa14bbe4689b9fb62e45f0a7
Leonardo Lastra Abufarne <leonardo.lastra@gmail.com>
2026-06-17 19:25:58 -0400

    corrige deploy

--- infra/docker/config/hydra/entrypoint.sh
+++ infra/docker/config/hydra/entrypoint.sh
@@ -3,6 +3,6 @@ set -eu  # ‘-e’ para salir en error, ‘-u’ para variables no definidas
 
 export POSTGRES_PASSWORD=$(cat "$POSTGRES_PASSWORD_FILE")
 export SECRETS_SYSTEM=$(cat "$SECRETS_SYSTEM_FILE")
-export DNS="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=disable&options=-c%20search_path%3Dmarketplace"
+export DSN="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=disable&options=-c%20search_path%3Dmarketplace"
 
 exec hydra serve all --dev
