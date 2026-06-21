534169eed0b51741b40b73e16a70615e705b0e55
Leonardo Lastra Abufarne <leonardo.lastra@gmail.com>
2026-06-21 16:15:09 -0400

    test workflow

--- front/src/layout/Footer.tsx
+++ front/src/layout/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
                         <use xlinkHref="#bootstrap"></use>
                     </svg>
                 </a>
-                <span className="mb-3 mb-md-0 text-body-secondary">© 2025 Company, Inc</span>
+                <span className="mb-3 mb-md-0 text-body-secondary">© 2026 Company, Inc</span>
             </div>
             <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
                 <li className="ms-3">
