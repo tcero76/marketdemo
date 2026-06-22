08de1ad1d78c1a98e46f8cf89a36a6fdcf15b48d
Leonardo Lastra Abufarne <leonardo.lastra@gmail.com>
2026-06-22 17:32:10 -0400

    add user

---
+++ flyway/sql/seed/R__create_user.up.sql
@@ -0,0 +1,4 @@
+DELETE FROM marketplace.users;
+
+insert into marketplace.users(user_id, nombre, password, roles,avatar, provider) values('123e4567-e89b-12d3-a456-426614174000', 'leonardo', 'random','streamer','https://lh3.googleusercontent.com/ogw/AF2bZyi3jj_bNU_05Z6ldnz_Xs-ofC1aGtz2o_GOyDO5RkSLHvk=s32-c-mo','internal');
+insert into marketplace.users(user_id, nombre, password, roles,avatar, provider) values('550e8400-e29b-41d4-a716-446655440000', 'andres', 'random', 'streamer','https://lh3.googleusercontent.com/ogw/AF2bZyi3jj_bNU_05Z6ldnz_Xs-ofC1aGtz2o_GOyDO5RkSLHvk=s32-c-mo','internal');
