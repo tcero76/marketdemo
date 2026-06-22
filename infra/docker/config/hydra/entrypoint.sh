1852f1d6936f9809d9eb80dd0d80ad89b1705f54
Leonardo Lastra Abufarne <leonardo.lastra@gmail.com>
2026-06-22 19:02:53 -0400

    test workflow

--- infra/docker/config/hydra/entrypoint.sh
+++ infra/docker/config/hydra/entrypoint.sh
@@ -5,4 +5,4 @@ export POSTGRES_PASSWORD=$(cat "$POSTGRES_PASSWORD_FILE")
 export SECRETS_SYSTEM=$(cat "$SECRETS_SYSTEM_FILE")
 export DSN="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=disable&options=-c%20search_path%3Dhydra"
 
-exec hydra serve all --dev
+exec hydra serve all
