91540325b549ee03b39d141496af8961367be1c9
Leonardo Lastra Abufarne <leonardo.lastra@gmail.com>
2026-07-31 12:31:03 -0400

    limpieza

--- flyway/sql/migration/V0_0_5__constrains.up.sql
+++ flyway/sql/migration/V0_0_5__constrains.up.sql
@@ -6,9 +6,3 @@ ALTER TABLE scrap.modelos ADD CONSTRAINT modelos_pkey PRIMARY KEY (id,id_job);
 
 ALTER TABLE scrap.posts DROP CONSTRAINT posts_pkey;
 ALTER TABLE scrap.posts ADD CONSTRAINT posts_pkey PRIMARY KEY (id,id_job);
-
-ALTER TABLE marketplace.modelos DROP CONSTRAINT modelos_pkey;
-ALTER TABLE marketplace.modelos ADD CONSTRAINT modelos_pkey PRIMARY KEY (id);
-
-ALTER TABLE marketplace.posts DROP CONSTRAINT posts_pkey;
-ALTER TABLE marketplace.posts ADD CONSTRAINT posts_pkey PRIMARY KEY (id);
