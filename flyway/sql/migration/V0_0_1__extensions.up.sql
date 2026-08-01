ccfa1e4beba81cc74b516dcdec348872f33567f0
Leonardo Lastra Abufarne <leonardo.lastra@gmail.com>
2026-08-01 09:13:25 -0400

    ajusta search_path

--- flyway/sql/migration/V0_0_1__extensions.up.sql
+++ flyway/sql/migration/V0_0_1__extensions.up.sql
@@ -5,3 +5,5 @@ ALTER SCHEMA extension OWNER TO tcero;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA extension;
 
 CREATE EXTENSION IF NOT EXISTS pg_stat_statements WITH SCHEMA public;
+
+ALTER ROLE tcero SET search_path TO extension, marketplace, scrap, hydra, marketplacedemo, chat, public;
