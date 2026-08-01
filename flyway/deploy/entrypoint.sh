c9a82ddd39bf397b001763aef73423a623603b2a
Leonardo Lastra Abufarne <leonardo.lastra@gmail.com>
2026-08-01 09:37:40 -0400

    corrige config flyway

--- flyway/deploy/entrypoint.sh
+++ flyway/deploy/entrypoint.sh
@@ -3,6 +3,12 @@
 set -e
 
 export FLYWAY_PASSWORD=$(cat /run/secrets/postgres_password)
+export FLYWAY_URL=jdbc:postgresql://${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}
+export FLYWAY_USER=${POSTGRES_USER}
+export FLYWAY_SCHEMAS=marketplace,scrap,hydra,marketplacedemo,chat,extension
+export FLYWAY_LOCATIONS=filesystem:/flyway/sql/migration,filesystem:/flyway/sql/seed
+export FLYWAY_PLACEHOLDERS_URL_EXTERNAL=${PROTOCOL_EXTERNAL}://${HOST_EXTERNAL}
+
 echo "⏳ Esperando que la DB esté lista..."
 until pg_isready \
   -h "$POSTGRES_HOST" \
